75d0e3aba2064ef8273ba230f9d9342cb7ebc79d
Isak Folke <isvack@gmail.com>
2025-04-08 11:29:57 +0200

    Ryan Gosling i databasen

--- src/main/groovy/org/example/model/User.groovy
+++ src/main/groovy/org/example/model/User.groovy
@@ -1,4 +1,29 @@
 package org.example.model
 
+import org.bson.Document
+
 class User {
+    String _id
+    String username
+    String password
+    Date registerDate
+    Integer exp
+
+    User(String username, String password, Date registerDate, Integer exp) {
+        this.username = username
+        this.password = password
+        this.registerDate = registerDate
+        this.exp = exp
+    }
+
+    void set_id(String _id) {
+        this._id = _id
+    }
+
+    def createDocument() {
+        return new Document("user", this.username)
+        .append("password", this.password)
+        .append("registerDate", this.registerDate)
+        .append("exp", this.exp)
+    }
 }
