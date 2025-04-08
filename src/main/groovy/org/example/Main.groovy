fd44cbc6535db88eeba02812a09a886a2164bf09
Isak Folke <isvack@gmail.com>
2025-04-08 10:20:28 +0200

    Initial commit 2

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -1,6 +1,10 @@
 package org.example
 
+import org.example.service.MongoService
+
 static void main(String[] args) {
     println "Hello world!"
     println("Hello Groovy")
+
+    def mongo = MongoService.connect()
 }
--- src/main/groovy/org/example/service/MongoService.groovy
+++ src/main/groovy/org/example/service/MongoService.groovy
@@ -1,4 +1,43 @@
 package org.example.service
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.MongoException
+import com.mongodb.ServerApi
+import com.mongodb.ServerApiVersion
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoDatabase
+import org.bson.BsonDocument
+import org.bson.BsonInt64
+import org.bson.Document
+import org.bson.conversions.Bson
+
 class MongoService {
+    static connect() {
+        def props = new Properties()
+        def envFile = new File(".env")
+        envFile.withInputStream { it -> props.load(it) }
+
+        def uri = props.getProperty("CONNECTION_STRING")
+
+        ServerApi serverApi = ServerApi.builder()
+        .version(ServerApiVersion.V1)
+        .build()
+
+         try (MongoClient mongoClient = MongoClients.create(
+                MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .build()
+        )) {
+             MongoDatabase database = mongoClient.getDatabase("admin")
+            try {
+                Bson command = new BsonDocument("ping", new BsonInt64(1))
+                Document commandResult = database.runCommand(command)
+                println 'Success!'
+            } catch (MongoException me) {
+                System.err.println(me)
+            }
+         }
+    }
 }
